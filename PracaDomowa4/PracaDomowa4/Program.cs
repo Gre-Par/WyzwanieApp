1706487454793d45294cf585b67c149316c62c75
Greta Pardyka <pardykag@gmail.com>
2023-04-25 12:21:50 +0200

    PracaDomow4

---
+++ PracaDomowa4/PracaDomowa4/Program.cs
@@ -0,0 +1,29 @@
+﻿string sex = "kobieta";
+int age = 24;
+string name = "Ewa";
+int ageEwa = 33;
+int ageKuba = 15;
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("To nie jest kobieta poniżej 30 lat");
+}
+if (name == "Ewa" && ageEwa == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine("To ni ejest Ewa i nie ma 33 lat");
+}
+if (ageKuba < 18 && sex != "mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("To nie jest niepełnoletni Mężczyna");
+}
